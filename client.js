caf17495c17e0ba18c759573784035fdc451fc61
Israel <israel.aguiar@gmail.com>
2020-08-07 14:30:08 -0300

    16.5. Entendendo o funcionamento de preflight do CORS

--- client.js
+++ client.js
@@ -4,7 +4,18 @@ function consultarRestaurantes() {
     type: "get",
 
     success: function(response) {
-      $("#conteudo").text(response);
+      $("#conteudo").text(JSON.stringify(response));
+    }
+  });
+}
+
+function fecharRestaurante() {
+  $.ajax({
+    url: "http://api.algafood.local:8080/restaurantes/1/fechamento",
+    type: "put",
+
+    success: function(response) {
+      alert("Restaurante foi fechado!");
     }
   });
 }
